c887dc2de2273c6b93b60fe5b789865512fe51e4
Ganagasabarinath V N <ganagasabarinath@gmail.com>
2026-07-14 13:54:59 +0530

    Create 8-StringToIntegerAtoi

---
+++ 8-StringToIntegerAtoi/8-StringToIntegerAtoi.py
@@ -0,0 +1,34 @@
+# Last updated: 7/14/2026, 1:54:59 PM
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
+        i = 0
+        n = len(s)
+
+        # Skip leading spaces
+        while i < n and s[i] == ' ':
+            i += 1
+
+        # Handle sign
+        sign = 1
+        if i < n and (s[i] == '+' or s[i] == '-'):
+            if s[i] == '-':
+                sign = -1
+            i += 1
+
+        result = 0
+
+        # Process digits
+        while i < n and s[i].isdigit():
+            digit = int(s[i])
+
+            # Check overflow
+            if result > (INT_MAX - digit) // 10:
+                return INT_MAX if sign == 1 else INT_MIN
+
+            result = result * 10 + digit
+            i += 1
+
+        return sign * result
