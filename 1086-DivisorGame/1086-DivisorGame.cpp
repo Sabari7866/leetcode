802e26d7ad45c0617008b050004e2ecbfb12ec46
Ganagasabarinath V N <ganagasabarinath@gmail.com>
2026-07-14 13:53:37 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.cpp
@@ -0,0 +1,11 @@
+// Last updated: 7/14/2026, 1:53:38 PM
+class Solution {
+public:
+    bool divisorGame(int n) {
+        if(n%2==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
