ef9f0444c487e74d7a5f0d88e90316e3f6124b93
Ganagasabarinath V N <ganagasabarinath@gmail.com>
2026-07-14 13:54:57 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,16 @@
+// Last updated: 7/14/2026, 1:54:55 PM
+class Solution {
+    public boolean isPalindrome(int x) {
+         if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int reversed=0;
+        int original=x;
+        while (x > 0) {
+            int digit = x % 10;
+            reversed=reversed * 10 + digit;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+}
