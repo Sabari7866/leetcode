28cb8254c58bba01374e1a0954f20b262c16c6b5
Ganagasabarinath V N <ganagasabarinath@gmail.com>
2026-07-15 14:41:21 +0530

    Create 4135-ConcatenateNonZeroDigitsAndMultiplyBySumI

---
+++ 4135-ConcatenateNonZeroDigitsAndMultiplyBySumI/4135-ConcatenateNonZeroDigitsAndMultiplyBySumI.java
@@ -0,0 +1,16 @@
+// Last updated: 7/15/2026, 2:41:20 PM
+class Solution {
+    public long sumAndMultiply(int n) {
+        int p = 1;
+        int x = 0, s = 0;
+        for (; n > 0; n /= 10) {
+            int v = n % 10;
+            s += v;
+            if (v != 0) {
+                x += p * v;
+                p *= 10;
+            }
+        }
+        return 1L * x * s;
+    }
+}
