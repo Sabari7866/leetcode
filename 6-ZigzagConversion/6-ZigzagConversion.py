755c19e490a4415eb2686d2d1b7c1d013cf04b23
Ganagasabarinath V N <ganagasabarinath@gmail.com>
2026-09-02 13:49:39 +0530

    Create 6-ZigzagConversion

---
+++ 6-ZigzagConversion/6-ZigzagConversion.py
@@ -0,0 +1,22 @@
+# Last updated: 9/2/2026, 1:49:40 PM
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [""] * numRows
+
+        current_row = 0
+        direction = 1
+
+        for ch in s:
+            rows[current_row] += ch
+
+            if current_row == 0:
+                direction = 1
+            elif current_row == numRows - 1:
+                direction = -1
+
+            current_row += direction
+
+        return "".join(rows)
