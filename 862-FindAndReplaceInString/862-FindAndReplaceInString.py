026e4eb24ef97ee0e888becc897f1817c9da3bc5
Ganagasabarinath V N <ganagasabarinath@gmail.com>
2026-09-02 13:46:52 +0530

    Create 862-FindAndReplaceInString

---
+++ 862-FindAndReplaceInString/862-FindAndReplaceInString.py
@@ -0,0 +1,20 @@
+# Last updated: 9/2/2026, 1:46:53 PM
+class Solution:
+    def findReplaceString(self, s, indices, sources, targets):
+        match = {}
+        for i, idx in enumerate(indices):
+            if s.startswith(sources[i], idx):
+                match[idx] = i
+                
+        res = []
+        i = 0
+        while i < len(s):
+            if i in match:
+                op_idx = match[i]
+                res.append(targets[op_idx])
+                i += len(sources[op_idx])
+            else:
+                res.append(s[i])
+                i += 1
+                
+        return "".join(res)
