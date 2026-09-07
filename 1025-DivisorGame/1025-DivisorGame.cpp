9fb442abc3cedaa48d6790429bf491b272bdda14
Ganagasabarinath V N <ganagasabarinath@gmail.com>
2026-09-07 12:16:46 +0530

    Create 1025-DivisorGame

---
+++ 1025-DivisorGame/1025-DivisorGame.cpp
@@ -0,0 +1,11 @@
+// Last updated: 9/7/2026, 12:16:48 PM
+class Solution {
+public:
+    bool divisorGame(int n) {
+        if(n%2==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
