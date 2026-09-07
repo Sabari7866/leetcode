ccbe896ede7e35faa013709f5db298efed62a10c
Ganagasabarinath V N <ganagasabarinath@gmail.com>
2026-09-07 12:17:10 +0530

    Create 771-JewelsAndStones

---
+++ 771-JewelsAndStones/771-JewelsAndStones.java
@@ -0,0 +1,14 @@
+// Last updated: 9/7/2026, 12:17:12 PM
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        for(int i=0;i<jewels.length();i++){
+            for(int j=0;j<stones.length();j++){
+                if(jewels.charAt(i)==stones.charAt(j)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
